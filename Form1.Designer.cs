1b8f7c0dfcb2a48a974271f52e8e407a0e9bd668
FigureScatingLover <lodochkina2001victoria@gmail.com>
2021-09-20 18:12:40 +0300

    last commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace WindowsFormsApp1._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(692, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Лабораторная работа №1 \"Основы программирования графики\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(692, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Вариант 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(692, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Лодочкина В. К.";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(888, 174);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Скрыть ось Ox";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(888, 197);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(101, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Скрыть ось Oy";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(888, 220);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(95, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Скрыть сетку";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(888, 243);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(108, 17);
+            this.checkBox4.TabIndex = 6;
+            this.checkBox4.Text = "Скрыть линейку";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(929, 439);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(67, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(692, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Строфоида";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(814, 391);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Параметр a";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(854, 446);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Шаг*";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(550, 550);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(888, 534);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(911, 384);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 21;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(911, 318);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 23;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(814, 325);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Масштаб";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(654, 472);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(398, 13);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "*Если значение предусматривает дробную часть, то она отделяется запятой";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.ForeColor = System.Drawing.Color.Maroon;
+            this.label9.Location = new System.Drawing.Point(657, 505);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 614);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace WindowsFormsApp1._1
+{
+    public partial class Form1 : Form
+    {
+        /*private List<double> x_dots;
+        private List<double> y_plus;
+        private List<double> y_minus;
+        private List<double> y_all;*/
+        private double x_start = -10;
+        private double x_end = 10;
+        private double y_start = -10;
+        private double y_end = 10;
+        private double step;
+        private double a;
+
+        private void yAxis()
+        {
+            Pen myPen = new Pen(Color.Black, 2f);
+            Graphics g;
+            g = pictureBox1.CreateGraphics();
+            g.DrawLine(myPen, 275, 0, 275, 550);
+            g.DrawLine(myPen, 275, 0, 268, 7);
+            g.DrawLine(myPen, 275, 0, 282, 7);
+            Font drawFont = new Font("Arial", 9);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            g.DrawString("Y", drawFont, drawBrush, 255, 10);
+        }
+
+        private void xAxis()
+        {
+            Pen myPen = new Pen(Color.Black, 2f);
+            Graphics g;
+            g = pictureBox1.CreateGraphics();
+            g.DrawLine(myPen, 0, 275, 550, 275);
+            g.DrawLine(myPen, 548, 275, 541, 268);
+            g.DrawLine(myPen, 548, 275, 541, 282);
+            Font drawFont = new Font("Arial", 9);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            g.DrawString("X", drawFont, drawBrush, 535, 285);
+        }
+
+        private void grid()
+        {
+            Pen myPen = new Pen(Color.Black, 1f);
+            Graphics g;
+            g = pictureBox1.CreateGraphics();
+            for (int i=0; i<20; i++)
+            {
+                g.DrawLine(myPen, (int)(0 + i * 27.5), 0, (int)(0 + i * 27.5), 550);
+            }
+
+            for (int i=0; i<20; i++)
+            {
+                g.DrawLine(myPen, 0, (int)(0+i*27.5), 550, (int)(0+i*27.5));
+            }     
+        }
+
+        private void ruler()
+        {
+            Pen myPen = new Pen(Color.Black, 2f);
+            Graphics g;
+            g = pictureBox1.CreateGraphics();
+            for (int i = 0; i < 20; i++)
+            {
+                g.DrawLine(myPen, (int)(0 + i * 27.5), 270, (int)(0 + i * 27.5), 280);
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                g.DrawLine(myPen, 270, (int)(0 + i * 27.5), 280, (int)(0 + i * 27.5));
+            }
+            int scale = Convert.ToInt32(numericUpDown2.Value);
+            Font drawFont = new Font("Arial", 9);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            for (int i=0; i<10; i++)
+            {
+
+                g.DrawString(num(i, scale), drawFont, drawBrush, (int)(275+i*27.5), 285); //x+
+                g.DrawString(num(i-10, scale), drawFont, drawBrush, (int)(0 + i * 27.5), 285); //x-
+
+                if (i == 0)
+                {
+                    continue;
+                }
+                g.DrawString(num(i, scale), drawFont, drawBrush, 260, (int)(275-i*27.5)); //y+
+                g.DrawString(num(-i, scale), drawFont, drawBrush, 255, (int)(275+i*27.5)); //y-
+            }
+        }
+
+        private String num(int i, int scale)
+        {
+            return Convert.ToString(scale * i);
+        }
+
+        private int xc(double p)
+        {
+            int x = (int)((550 / (x_end - (x_start))) * p - (550 / (x_end - (x_start))) * (x_start));
+            return x;
+        }
+
+        private int yc(double p)
+        {
+
+            int y = (int)((-550 / (y_end - (y_start))) * p + (550 / (y_end - (y_start))) * y_end);
+            return y;
+        }
+
+        private double myRound(double x, int precision)
+        {
+            return ((int)(x * Math.Pow(10.0, precision)) / Math.Pow(10.0, precision));
+        }
+
+        private void plot()
+        {
+            label9.Text = "";
+            Pen myPen = new Pen(Color.Blue, 1.5f);
+            Graphics g;
+            g = pictureBox1.CreateGraphics();
+            g.Clear(SystemColors.Control);
+            //int sc = Convert.ToInt16(numericUpDown2.Value);
+            x_start = x_start * (Convert.ToDouble(numericUpDown2.Value));
+            x_end = x_end * (Convert.ToDouble(numericUpDown2.Value));
+            y_start = y_start * (Convert.ToDouble(numericUpDown2.Value));
+            y_end = y_end * (Convert.ToDouble(numericUpDown2.Value));
+            if (!(checkBox1.Checked))
+            {
+                xAxis();
+            }
+            if (!(checkBox2.Checked))
+            {
+                yAxis();
+            }
+            if (!(checkBox3.Checked))
+            {
+                grid();
+            }
+            if (!(checkBox4.Checked))
+            {
+                ruler();
+            }
+            a = Convert.ToDouble(numericUpDown1.Value);
+            string stepstr=textBox4.Text;
+            step = 0.1;
+            if (stepstr != "") {
+                step = Convert.ToDouble(stepstr);
+            }
+            if (step < 0.03)
+            {
+                label9.Text = "Укажите значение шага больше, чем 0,03";
+            }
+            int count = 0;
+                for (double i = -Math.Abs(a); i < Math.Abs(a); i += step) ///////////////
+                {
+                    if (a != 0)
+                    {
+                        count++;
+                    }
+                }
+            //count *= 2;
+            Point[] points = new Point[count];
+            Point[] points2 = new Point[count];
+            int num_point = 0;
+            bool flag = false;
+            if (a == 0)
+            {
+                label9.Text = "Значение параметра а не может быть равно 0!";
+            }
+            if ((a > 0)&& (step >= 0.03))
+            {
+                for (double i = -a; i < a; i = myRound(i+step, 2))
+                {
+                    if (a != 0)
+                    {
+                        if (num_point == count)
+                        {
+                            break;
+                        }
+
+                        points[num_point] = new Point(xc(i), yc(i * Math.Sqrt((a + i) / (a - i))));
+                        points2[num_point] = new Point(xc(i), yc(-i * Math.Sqrt((a + i) / (a - i))));
+                        num_point++;
+                        flag = true;
+                        //label5.Text = Convert.ToString(step)+" "+Convert.ToString(a);
+                    }
+                }
+
+                points[count - 1] = new Point((xc(a - 0.001)), (yc((((a - 0.001) * Math.Sqrt(a + a - 0.001)) / (Math.Sqrt(a - a + 0.001)))))/*/sc*/);
+                points2[count - 1] = new Point((xc(a - 0.001)), /*-*/(yc(((-(a - 0.001) * Math.Sqrt(a + a - 0.001)) / (Math.Sqrt(a - a + 0.001)))))/*/sc*/);
+            }
+            if ((a < 0)&&(step>=0.03))
+            {
+                a = -a;
+                for (double i = -a; i < a; i = myRound(i + step, 2))
+                {
+                    if (a != 0)
+                    {
+                        if (num_point == count)
+                        {
+                            break;
+                        }
+                        points[num_point] = new Point(xc(-i), yc(i * Math.Sqrt((a + i) / (a - i))));
+                        points2[num_point] = new Point(xc(-i), yc(-i * Math.Sqrt((a + i) / (a - i))));
+
+                        num_point++;
+                        flag = true;
+                    }
+                }
+                points[count - 1] = new Point((xc(a + 0.0001)), (yc((a + 0.0001) * Math.Sqrt((a + a + 0.0001) / (a - a + 0.0001))))/*/sc*/);
+                points2[count - 1] = new Point((xc(a + 0.0001)), (yc(-(a + 0.0001) * Math.Sqrt((a + a + 0.0001) / (a - a + 0.0001))))/*/sc*/);
+            }
+
+
+            if (flag)
+    {
+        g.DrawLines(myPen, points);
+        g.DrawLines(myPen, points2);
+                //g.DrawLine(myPen, points[0], points2[0]);
+                x_start = x_start / (Convert.ToDouble(numericUpDown2.Value));
+                x_end = x_end / (Convert.ToDouble(numericUpDown2.Value));
+                y_start = y_start / (Convert.ToDouble(numericUpDown2.Value));
+                y_end = y_end / (Convert.ToDouble(numericUpDown2.Value));
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e) //step
+        {
+            string tb4 = textBox4.Text;
+            if (tb4 != "")
+            {
+                step = Convert.ToDouble(tb4);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            yAxis();
+            xAxis();
+            grid();
+            ruler();
+            plot();
+        }
+        private void pictureBox1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+        }
+        private void settings()
+        {
+            Graphics g;
+            g = pictureBox1.CreateGraphics();
+            g.Clear(SystemColors.Control);
+            if (!(checkBox1.Checked))
+            {
+                xAxis();
+            }
+            if (!(checkBox2.Checked))
+            {
+                yAxis();
+            }
+            if (!(checkBox3.Checked))
+            {
+                grid();
+            }
+            if (!(checkBox4.Checked))
+            {
+                ruler();
+            }
+            if (textBox4.Text != "")
+            {
+                plot();
+            }
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) //hide Ox
+        {
+            settings();
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e) //hide Oy
+        {
+            settings();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e) //hide grid
+        {
+            settings();
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e) //hide ruler
+        {
+            settings();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            yAxis();
+            xAxis();
+            grid();
+            ruler();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            /*x_start = x_start * (Convert.ToDouble(numericUpDown2.Value));
+            x_end = x_end * (Convert.ToDouble(numericUpDown2.Value));
+            y_start = y_start * (Convert.ToDouble(numericUpDown2.Value));
+            y_end = y_end * (Convert.ToDouble(numericUpDown2.Value));*/
+            settings();
+            /*x_start = x_start / (Convert.ToDouble(numericUpDown2.Value));
+            x_end = x_end / (Convert.ToDouble(numericUpDown2.Value));
+            y_start = y_start / (Convert.ToDouble(numericUpDown2.Value));
+            y_end = y_end / (Convert.ToDouble(numericUpDown2.Value));*/
+        }
+    }
+}
